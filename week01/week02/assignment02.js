502810d6c22957ef15c629a54966d978b6c1e815
Royal_Telivision <gwenom64@gmail.com>
2023-02-02 18:59:33 -0500

    week06 work as Java :D

---
+++ week01/week02/assignment02.js
@@ -0,0 +1,13 @@
+// other types of JavaScript have tuples, but not node.js
+// mytup = #["cat", "dog", "cow"]
+mylist = ["whale", "fish", "shark"]
+
+myset = new Set()
+
+myset.add("one")
+myset.add("two")
+myset.add("three")
+
+//console.log(mytup)
+console.log(mylist)
+console.log(myset)
